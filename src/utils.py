83f41c0ecd44df1d9f61598d606c8bb9915073b2
Partha Kalita <paka05@dfki.de>
2024-07-29 11:47:28 +0200

    Your commit message

--- src/utils.py
+++ src/utils.py
@@ -41,8 +41,12 @@ def divide(a: float, b: float) -> float:
     Returns:
     float
     '''
-    return a / b
-
+    try:
+        return a / b
+    except ZeroDivisionError:
+        print("Error: Division by zero is not allowed.")
+        return None
+    
 def modulo(a: int, b: int):
     '''
     ...
